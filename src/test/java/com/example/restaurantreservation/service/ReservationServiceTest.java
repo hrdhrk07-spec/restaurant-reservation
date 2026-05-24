a137d52e137a102fd44bce1131df79a88403594a
Hiroki Harada <hrdhrk07@gmail.com>
2026-05-24 17:01:39 +0900

    test: テスト設計書・テストコードの追加完了(ReservationService)
    refactor: 不要なクラスファイルの削除

--- src/test/java/com/example/restaurantreservation/service/ReservationServiceTest.java
+++ src/test/java/com/example/restaurantreservation/service/ReservationServiceTest.java
@@ -1,15 +1,21 @@
 package com.example.restaurantreservation.service;
 
-import com.example.restaurantreservation.entity.Holiday;
+import com.example.restaurantreservation.entity.*;
 import com.example.restaurantreservation.enums.HolidayDayOfWeek;
+import com.example.restaurantreservation.form.ReservationForm;
 import com.example.restaurantreservation.repository.HolidayRepository;
 import com.example.restaurantreservation.repository.ReservationRepository;
 import com.example.restaurantreservation.repository.SeatDetailRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.*;
 import java.util.ArrayList;
@@ -18,33 +24,37 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
 
+    // テスト対象
+    @Spy
+    @InjectMocks
     private ReservationService reservationService;
-    private ReservationRepository reservationRepository;
-    private SeatDetailRepository seatDetailRepository;
-    private HolidayRepository holidayRepository;
-    private Clock clock;
 
-    @BeforeEach
-    void setUp() {
-        this.reservationRepository = mock(ReservationRepository.class);
-        this.seatDetailRepository = mock(SeatDetailRepository.class);
-        this.holidayRepository = mock(HolidayRepository.class);
-        // 第１引数はUTC基準の時刻、第２引数は表示するタイムゾーンなので10時を指定
-        this.clock = Clock.fixed(Instant.parse("2026-05-20T10:00:00Z"), ZoneId.of("Asia/Tokyo"));
-        this.reservationService = new ReservationService(reservationRepository, seatDetailRepository, holidayRepository, clock);
-    }
+    // モック
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @Mock
+    private SeatDetailRepository seatDetailRepository;
+
+    @Mock
+    private HolidayRepository holidayRepository;
+
+    // スパイ
+    @Spy
+    // 第１引数はUTC基準の時刻、第２引数は表示するタイムゾーンなので10時を指定
+    private Clock clock = Clock.fixed(Instant.parse("2026-05-20T10:00:00Z"), ZoneId.of("Asia/Tokyo"));
 
     @ParameterizedTest
     @MethodSource("isHolidayProvider")
     @DisplayName("定休日チェック")
     void isHoliday(LocalDateTime reservedAt, List<Holiday> holidays, boolean expected) {
-        when(holidayRepository.findByRestaurantId(anyLong())).thenReturn(holidays);
+        when(holidayRepository.findByRestaurantId(any())).thenReturn(holidays);
         assertEquals(expected, reservationService.isHoliday(0L, reservedAt));
     }
 
@@ -135,6 +145,161 @@ class ReservationServiceTest {
                 arguments(LocalDateTime.of(2026, 5, 20, 18, 59, 59), true),
                 arguments(LocalDateTime.of(2026, 5, 20, 19, 0, 1), false)
         );
+    }
+
+    @Test
+    @DisplayName("過去日時への予約")
+    void saveReservation_isPastDate() {
+
+        // 後述のチェックは予約日時にかかわらず結果を返すため、ここでは予約日時を設定しない
+        ReservationForm reservationForm = new ReservationForm();
+
+        // isPastDateがtrueを返すようにモック
+        doReturn(true).when(reservationService).isPastDate(any());
+
+        // 期待結果
+        assertThrows(
+                RuntimeException.class,
+                () -> reservationService.saveReservation(mock(User.class), mock(SeatDetail.class), reservationForm)
+        );
+    }
+
+    @Test
+    @DisplayName("定休日への予約")
+    void saveReservation_isHoliday() {
+
+        // 後述のチェックは予約日時にかかわらず結果を返すため、ここでは予約日時を設定しない
+        ReservationForm reservationForm = new ReservationForm();
+
+        // 席詳細がレストランを返せるようにモック
+        Restaurant restaurant = mock(Restaurant.class);
+        SeatDetail seatDetail = mock(SeatDetail.class);
+        when(seatDetail.getRestaurant()).thenReturn(restaurant);
+
+        // isPastDateがfalseを返すようにモック
+        doReturn(false).when(reservationService).isPastDate(any());
+
+        // isHolidayがtrueを返すようにモック
+        doReturn(true).when(reservationService).isHoliday(any(), any());
+
+        // 期待結果
+        assertThrows(
+                RuntimeException.class,
+                () -> reservationService.saveReservation(mock(User.class), seatDetail, reservationForm)
+        );
+
+    }
+
+    @Test
+    @DisplayName("受付時間外への予約")
+    void saveReservation_canReception() {
+
+        // 席詳細がレストランを返せるようにモック
+        Restaurant restaurant = mock(Restaurant.class);
+        SeatDetail seatDetail = mock(SeatDetail.class);
+        when(seatDetail.getRestaurant()).thenReturn(restaurant);
+
+        // このケースではcanReceptionがfalseを返すように予約日時を設定
+        ReservationForm reservationForm = new ReservationForm();
+        reservationForm.setReservedAt(LocalDateTime.of(2026,5,20,15,0,0));
+
+        // isPastDateがfalseを返すようにモック
+        doReturn(false).when(reservationService).isPastDate(any());
+
+        // isHolidayがfalseを返すようにモック
+        doReturn(false).when(reservationService).isHoliday(any(), any());
+
+        // canReceptionがfalseを返すように受付時間をモック
+        when(restaurant.getReceptionStartTime()).thenReturn(LocalTime.of(19,0));
+        when(restaurant.getReceptionEndTime()).thenReturn(LocalTime.of(22,0));
+
+        // 期待結果
+        assertThrows(
+                RuntimeException.class,
+                () -> reservationService.saveReservation(mock(User.class), seatDetail, reservationForm)
+        );
+
+    }
+
+    @Test
+    @DisplayName("席重複チェックNG")
+    void saveReservation_noSeats() {
+
+        // 席詳細がレストランを返せるようにモック
+        Restaurant restaurant = mock(Restaurant.class);
+        SeatDetail seatDetail = mock(SeatDetail.class);
+        when(seatDetail.getRestaurant()).thenReturn(restaurant);
+
+        // このケースではcanReceptionがtrueを返すように予約日時を設定
+        ReservationForm reservationForm = new ReservationForm();
+        reservationForm.setReservedAt(LocalDateTime.of(2026,5,20,20,0,0));
+
+        // isPastDateがfalseを返すようにモック
+        doReturn(false).when(reservationService).isPastDate(any());
+
+        // isHolidayがfalseを返すようにモック
+        doReturn(false).when(reservationService).isHoliday(any(), any());
+
+        // canReceptionがtrueを返すように受付時間をモック
+        when(restaurant.getReceptionStartTime()).thenReturn(LocalTime.of(19,0));
+        when(restaurant.getReceptionEndTime()).thenReturn(LocalTime.of(22,0));
+
+        // 席詳細の席セット数で2を取得するようモック
+        when(seatDetail.getNumberOfSeats()).thenReturn(2);
+
+        // 重複している予約の数で2を取得するようモック
+        when(reservationRepository.countOverlapping(any(),any(),any(),any())).thenReturn(2);
+
+        // 期待結果
+        assertThrows(
+                RuntimeException.class,
+                () -> reservationService.saveReservation(mock(User.class), seatDetail, reservationForm)
+        );
+
+    }
+
+    @Test
+    @DisplayName("予約正常系")
+    void saveReservation_OK() {
+
+        // 席詳細がレストランを返せるようにモック
+        Restaurant restaurant = mock(Restaurant.class);
+        SeatDetail seatDetail = mock(SeatDetail.class);
+        when(seatDetail.getRestaurant()).thenReturn(restaurant);
+
+        // このケースではcanReceptionがtrueを返すように予約日時を設定
+        ReservationForm reservationForm = new ReservationForm();
+        reservationForm.setReservedAt(LocalDateTime.of(2026,5,20,20,0,0));
+
+        // isPastDateがfalseを返すようにモック
+        doReturn(false).when(reservationService).isPastDate(any());
+
+        // isHolidayがfalseを返すようにモック
+        doReturn(false).when(reservationService).isHoliday(any(), any());
+
+        // canReceptionがtrueを返すように受付時間をモック
+        when(restaurant.getReceptionStartTime()).thenReturn(LocalTime.of(19,0));
+        when(restaurant.getReceptionEndTime()).thenReturn(LocalTime.of(22,0));
+
+        // 席詳細の席セット数で2を取得するようモック
+        when(seatDetail.getNumberOfSeats()).thenReturn(2);
+
+        // 重複している予約の数で1を取得するようモック
+        when(reservationRepository.countOverlapping(any(),any(),any(),any())).thenReturn(1);
+
+        // 予約人数のセット
+        reservationForm.setNumberOfGuests(2);
+
+        // 予約をモック
+        Reservation reservation = mock(Reservation.class);
+
+        // 登録処理のモック
+        when(reservationRepository.save(any())).thenReturn(reservation);
+
+        // 期待結果
+        assertEquals(reservationService.saveReservation(mock(User.class), seatDetail, reservationForm), reservation);
+        verify(reservationRepository, times(1)).save(any());
+
     }
 
 }
